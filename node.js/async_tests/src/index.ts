2faae695e0d41a004d9cb098245fb8cfdafae60c
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-27 22:09:21 +0800

    Write sample tests of async function

--- node.js/async_tests/src/index.ts
+++ node.js/async_tests/src/index.ts
@@ -1,5 +1,5 @@
 /** Use Axios to get data from restcountries api */
-import axios from 'axios';
+import axios from "axios";
 
 /** Use the free API https://restcountries.eu/
  * You will use the following endpoints
@@ -8,12 +8,19 @@ import axios from 'axios';
  */
 
 /** Create getCountry Function here */
+async function getCountry(countryName: string) {
+  const getApi = await axios(
+    `https://restcountries.eu/rest/v2/name/${countryName}`
+  );
+  const data = getApi.data;
 
+  return data[0];
+}
 
 /** Create a test for this getRegion function */
-async function getRegionCountries(regionalbloc: string) {
+async function getRegionalBlocCountries(blocName: string) {
   const getApi = await axios(
-    `https://restcountries.eu/rest/v2/regionalbloc/${regionalbloc}`
+    `https://restcountries.eu/rest/v2/regionalbloc/${blocName}`
   );
   const data = getApi.data;
   const countries = [];
@@ -24,10 +31,20 @@ async function getRegionCountries(regionalbloc: string) {
 }
 
 /** Create getRegionCapitals function here */
-
+async function getRegionCapitals(regionName: string) {
+  const getApi = await axios(
+    `https://restcountries.eu/rest/v2/region/${regionName}`
+  );
+  const data = getApi.data;
+  const capitals = [];
+  for (let i = 0; i < data.length; i++) {
+    capitals.push(data[i].capital);
+  }
+  return capitals;
+}
 
 export default {
   getCountry,
-  getRegionCountries,
-  getRegionCapitals
+  getRegionalBlocCountries,
+  getRegionCapitals,
 };
--- node.js/async_tests/src/tests/indexSpec.ts
+++ node.js/async_tests/src/tests/indexSpec.ts
@@ -1,20 +1,29 @@
 import countries from "../index";
 
-it("should get basic data on the country canada", async () => {
-	const data = await countries.getCountry('canada');
-	expect(data).toEqual({
-		capital: 'Ottawa',
-		region: 'Americas',
-		numericCode: '124'
-	});
+describe("Function getCountry", () => {
+  it("should get basic data of the country Canada", async () => {
+    const data = await countries.getCountry("canada");
+    expect(data.capital).toEqual("Ottawa");
+    expect(data.region).toEqual("Americas");
+    expect(data.numericCode).toEqual("124");
+  });
 });
 
-/** Add test for getRegionCountries function here */
+describe("Function getRegionCapitals", () => {
+  it("should get list of capitals of ASIA countries", async () => {
+    const data = await countries.getRegionCapitals("asia");
+    expect(data.length).toBeGreaterThan(10);
+    expect(data).toContain("Beijing");
+  });
+});
 
-
-it("should get capitals of NAFTA countries", async () => {
-	const data = await countries.getRegionCapitals('nafta');
-	expect(data).toEqual([
-		'Ottawa', 'Mexico City', 'Washington, D.C.'
-	]);
+describe("Function getRegionalBlocCountries", () => {
+  it("should get list of ASEAN countries", async () => {
+    const data = await countries.getRegionalBlocCountries("asean");
+    expect(data.length).toEqual(10);
+    expect(data).toContain("Viet Nam");
+    expect(data).toContain("Singapore");
+    expect(data).toContain("Malaysia");
+    expect(data).toContain("Thailand");
+  });
 });
