b83f0b9d74d7d4b5aae0b8f9428d14119aa7279e
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-05-27 21:25:58 +0800

    Write sample standard test cases

--- node.js/standard_tests/src/tests/indexSpec.ts
+++ node.js/standard_tests/src/tests/indexSpec.ts
@@ -1,29 +1,36 @@
-import arrays from '../utilities/arrays.js';
-import numbers from '../utilities/numbers.js';
-import strings from '../utilities/strings.js';
+import arrays from "../utilities/arrays.js";
+import numbers from "../utilities/numbers.js";
+import strings from "../utilities/strings.js";
 
 const numArr = [3, 4, 5, 6];
-const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+const wordArr = ["cat", "dog", "rabbit", "bird"];
 const arrSum = arrays.addArr(numArr);
 const mixArr = arrays.concatArr(numArr, wordArr);
-const myNum = ('15' as unknown) as number % 2;
-const five = parseInt('5');
+const myNum = ("15" as unknown as number) % 2;
+const five = parseInt("5");
 
-describe('Removing third item from an array', () => {
-  it("return an array with third item remove", () => {
-      expect(arrays.cut3([1, 2, 3, 4])).toEqual([1, 2, 4])
-      expect(arrays.cut3(['1', '2', '3', '4'])).toEqual(['1', '2', '4'])
+describe("Removing third item from an array", () => {
+  it("returns an array with third item remove", () => {
+    expect(arrays.cut3([1, 2, 3, 4])).toEqual([1, 2, 4]);
+    expect(arrays.cut3(["1", "2", "3", "4"])).toEqual(["1", "2", "4"]);
   });
-    
-  it("return original array if there are less than 3 items", () => {
-      expect(arrays.cut3([1, 2])).toEqual([1, 2]);
-      expect(arrays.cut3([])).toEqual([]);      
-  });
-    
-})
 
-// console.log(arrays.cut3(mixArr));
-// console.log(numbers.sum(arrSum, myNum));
-// console.log(strings.capitalize('the quick brown fox'));
-// console.log(numbers.multiply(five, 8));
-// console.log(arrays.lgNum(mixArr));
+  it("returns original array if there are less than 3 items", () => {
+    expect(arrays.cut3([1, 2])).toEqual([1, 2]);
+    expect(arrays.cut3([])).toEqual([]);
+  });
+});
+
+describe("Adding two numbers", () => {
+  it("returns their sum if both numbers are non-negative", () => {
+    expect(numbers.sum(0, 10000000000)).toEqual(10000000000);
+  });
+
+  it("returns their sum if both numbers are non-positive", () => {
+    expect(numbers.sum(-1, -10000000000)).toEqual(-10000000001);
+  });
+
+  it("returns their sum if one number is positive and the other is negative", () => {
+    expect(numbers.sum(-1, 1)).toEqual(0);
+  });
+});
